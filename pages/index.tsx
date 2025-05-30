37fd9fddc841bfc05e68ac39ea8d65432a4ba939
Inner-shibora <mrnong.bnt3@gmail.com>
2025-05-30 22:53:40 +0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, textAlign: 'center' }}>
+      <h1>Welcome to Shibora AI Core</h1>
+      <p>Landing and Presale Site for SHIBORA Token</p>
+    </div>
+  )
+}
