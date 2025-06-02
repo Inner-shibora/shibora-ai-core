065dde5df3194ac4f6df0f2ee3b9393ef4a4fd99
Inner-shibora <mrnong.bnt3@gmail.com>
2025-06-02 07:17:53 +0700

    import '@/globals.css'; import type { AppProps } from 'next/app';
    import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
    import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
    import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
    import '@solana/wallet-adapter-react-ui/styles.css';
    
    const wallets = [new PhantomWalletAdapter()];
    
    export default function App({ Component, pageProps }: AppProps) {
      return (
        <ConnectionProvider endpoint="https://api.mainnet-beta.solana.com">
          <WalletProvider wallets={wallets} autoConnect>
            <WalletModalProvider>
              <Component {...pageProps} />
            </WalletModalProvider>
          </WalletProvider>
        </ConnectionProvider>
      );
    }

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import '@/globals.css';
+import type { AppProps } from 'next/app';
+
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+const wallets = [new PhantomWalletAdapter()];
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ConnectionProvider endpoint="https://api.mainnet-beta.solana.com">
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
